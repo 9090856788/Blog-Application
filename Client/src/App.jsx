856c6c9630e4efcf3afdcefaafa7769fc611b383
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-04-02 12:27:37 +0530

    Created required pages & added router functionality

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,64 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: (
+          <Link to="Home">Home</Link>
+      )
+  },
+
+  {
+    path: '/about',
+    element: (
+        <Link to="About">About Us</Link>
+    )
+},
+
+{
+  path: '/blogs',
+  element: (
+      <Link to="Blogs">Blogs</Link>
+  )
+},
+
+{
+  path: '/contact',
+  element: (
+      <Link to="Contact">Contact Us</Link>
+  )
+},
+
+{
+  path: '/login',
+  element: (
+      <Link to="Login">Login</Link>
+  )
+},
+
+{
+  path: '/register',
+  element: (
+      <Link to="Register">Register</Link>
+  )
+}
+])
 
 const App = () => {
   
   return (
     <>
-      App
+    <RouterProvider router={router} />
     </>
-  )
-}
+    )
+  }
 
 export default App;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom';
 import App from './App';
 import './index.css';
 
+
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(
