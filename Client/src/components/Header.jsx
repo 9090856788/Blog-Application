76d55412ee3784e60a6fef2f32379de4287e63ab
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-04-18 01:21:14 +0530

    install the flowbite package, Create the Header component & design the logo of the web page

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Navbar} from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Navbar } from "flowbite-react";
 
 const Header = () => {
   return (
     <>
-      <Navbar className='border-b-2'>
-        <Link to='/' className='text-wrap'>
-        
-        </Link>
+      <Navbar className="border-b-2">
+        <div className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-md text-white">
+            ZeroTech
+          </span>
+          Blog
+        </div>
       </Navbar>
     </>
-  )
-}
+  );
+};
 
 export default Header;
